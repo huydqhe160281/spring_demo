abdaa293c2a4cfc552c7a62de446402dcd2f135b
Huy Đỗ Quốc <huydq@kaopiz.com>
2025-02-04 11:18:56 +0700

    feat: login with hash pass2

---
+++ src/main/java/com/huy/identify_service/dto/request/IntrospectRequest.java
@@ -0,0 +1,14 @@
+package com.huy.identify_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectRequest {
+    String username;
+    String password;
+}
